3c28fa4f26a5c1571617ad400721f6248ef2658b
evanbai80 <evanbai80@gmail.com>
2025-07-17 15:03:38 -0400

    Add files via upload

--- Flappy Bird.py
+++ Flappy Bird.py
@@ -9,8 +9,8 @@ playerimage=pygame.Surface((32,32))
 
 toppipe_y=random.randint(0,300)
 bottompipe_y=362-toppipe_y
-toppipe=pygame.Rect((288,0),(130,toppipe_y))
-bottompipe=pygame.Rect((288,512-bottompipe_y),(130,bottompipe_y))
+toppipe=pygame.Rect((288,0),(40,toppipe_y))
+bottompipe=pygame.Rect((288,512-bottompipe_y),(40,bottompipe_y))
 topimage=pygame.Surface((40,toppipe_y))
 bottomimage=pygame.Surface((40,bottompipe_y))
 bottomimage.fill((255,255,255))
@@ -24,7 +24,7 @@ g=9.8
 dt=0.1
 v=0
 while True:
-    clock.tick(10)
+    clock.tick(15)
     screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -35,20 +35,22 @@ while True:
     toppipe.move_ip(-5,0)
     bottompipe.move_ip(-5,0)
     if toppipe.x<-50:
-        toppipe_y=random.randint(0,300)
-        bottompipe_y=362-toppipe_y
-        toppipe=pygame.Rect((288,0),(130,toppipe_y))
-        bottompipe=pygame.Rect((288,512-bottompipe_y),(130,bottompipe_y))
-        topimage=pygame.Surface((40,toppipe_y))
-        bottomimage=pygame.Surface((40,bottompipe_y))
-        bottomimage.fill((255,255,255))
-        topimage.fill((255,255,255))
+        o=random.randint(1,10)
+        if o<=5:            
+            toppipe_y=random.randint(0,300)
+            bottompipe_y=362-toppipe_y
+            toppipe=pygame.Rect((288,0),(40,toppipe_y))
+            bottompipe=pygame.Rect((288,512-bottompipe_y),(40,bottompipe_y))
+            topimage=pygame.Surface((40,toppipe_y))
+            bottomimage=pygame.Surface((40,bottompipe_y))
+            bottomimage.fill((255,255,255))
+            topimage.fill((255,255,255))
     if player.colliderect(toppipe) or player.colliderect(bottompipe):
         print("GAME OVER!")
         quit()
     v=v+g*dt
     player.move_ip(0,v)
-    print(v)
+    #print(v)
 
     screen.blit(topimage,toppipe)
     screen.blit(bottomimage,bottompipe)
