e680e46065e344b7c2be06cb6bd06fafd897fd05
evanbai80 <evanbai80@gmail.com>
2025-07-18 10:35:17 -0400

    Add files via upload

--- Flappy Bird.py
+++ Flappy Bird.py
@@ -7,15 +7,27 @@ screen=pygame.display.set_mode((288,512))
 player=pygame.Rect((50,216),(32,32))
 playerimage=pygame.Surface((32,32))
 
-toppipe_y=random.randint(0,300)
-bottompipe_y=362-toppipe_y
-toppipe=pygame.Rect((288,0),(40,toppipe_y))
-bottompipe=pygame.Rect((288,512-bottompipe_y),(40,bottompipe_y))
+tlist=[]
+blist=[]
+for i in range(3):
+    pipe_x=random.randint(288,400)
+    toppipe_y=random.randint(0,300)
+    bottompipe_y=362-toppipe_y
+    toppipe=pygame.Rect((pipe_x,0),(40,toppipe_y))
+    bottompipe=pygame.Rect((pipe_x,512-bottompipe_y),(40,bottompipe_y))
+    tlist.append(toppipe)
+    blist.append(bottompipe)
+    print (tlist[i])
+    print (blist[i])
+
 topimage=pygame.Surface((40,toppipe_y))
 bottomimage=pygame.Surface((40,bottompipe_y))
 bottomimage.fill((255,255,255))
 topimage.fill((255,255,255))
 
+slist=[]
+for q in range (6):
+    slist.append(topimage)
 
 clock=pygame.time.Clock()
 bird=pygame.image.load('bird.png')
@@ -23,6 +35,8 @@ bird=pygame.transform.scale(bird,(32,32))
 g=9.8
 dt=0.1
 v=0
+s=[0,0,0]
+t=[0,0,0]
 while True:
     clock.tick(15)
     screen.fill((0,0,0))
@@ -34,17 +48,33 @@ while True:
                 v=-15
     toppipe.move_ip(-5,0)
     bottompipe.move_ip(-5,0)
-    if toppipe.x<-50:
-        o=random.randint(1,10)
-        if o<=5:            
-            toppipe_y=random.randint(0,300)
-            bottompipe_y=362-toppipe_y
-            toppipe=pygame.Rect((288,0),(40,toppipe_y))
-            bottompipe=pygame.Rect((288,512-bottompipe_y),(40,bottompipe_y))
-            topimage=pygame.Surface((40,toppipe_y))
-            bottomimage=pygame.Surface((40,bottompipe_y))
-            bottomimage.fill((255,255,255))
-            topimage.fill((255,255,255))
+    for toppipe in tlist:
+        if toppipe.x<-50:
+            tlist.remove(toppipe)
+            o=random.randint(1,10)
+            if o<=5:            
+                for i in range(3):
+                    pipe_x=random.randint(288,400)
+                    s[i]=pipe_x
+                    toppipe_y=random.randint(0,300)
+                    t[i]=toppipe_y
+                    toppipe=pygame.Rect((pipe_x,0),(40,toppipe_y))
+                    tlist.append(toppipe)
+                
+                #topimage=pygame.Surface((40,toppipe_y))
+                #bottomimage=pygame.Surface((40,bottompipe_y))
+                #bottomimage.fill((255,255,255))
+                #topimage.fill((255,255,255))
+    for bottompipe in blist:
+        if bottompipe.x<-50:
+            blist.remove(bottompipe)
+            z=random.randint(1,10)
+            if z<=5:
+                for i in range (3):
+                    print (s[i])
+                    bottompipe_y=362-toppipe_y
+                    bottompipe=pygame.Rect((s[i],512-t[i]),(40,t[i]))
+                    blist.append(bottompipe)
     if player.colliderect(toppipe) or player.colliderect(bottompipe):
         print("GAME OVER!")
         quit()
@@ -52,7 +82,10 @@ while True:
     player.move_ip(0,v)
     #print(v)
 
-    screen.blit(topimage,toppipe)
-    screen.blit(bottomimage,bottompipe)
+    for toppipe in tlist:
+        screen.blit(topimage,toppipe)
+    for bottompipe in blist:
+        screen.blit(bottomimage,bottompipe)
+    
     screen.blit(bird,player.topleft)
     pygame.display.update()
