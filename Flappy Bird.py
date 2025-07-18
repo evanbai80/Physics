6d4744e1516ee7be297e08da74d6c67761c728f3
evanbai80 <evanbai80@gmail.com>
2025-07-18 12:15:28 -0400

    Add files via upload

--- Flappy Bird.py
+++ Flappy Bird.py
@@ -9,8 +9,8 @@ playerimage=pygame.Surface((32,32))
 
 tlist=[]
 blist=[]
-for i in range(3):
-    pipe_x=random.randint(288,400)
+for i in range(2):
+    pipe_x=random.randint(300,400)
     toppipe_y=random.randint(0,300)
     bottompipe_y=362-toppipe_y
     toppipe=pygame.Rect((pipe_x,0),(40,toppipe_y))
@@ -35,8 +35,8 @@ bird=pygame.transform.scale(bird,(32,32))
 g=9.8
 dt=0.1
 v=0
-s=[0,0,0]
-t=[0,0,0]
+s=[0,0]
+t=[0,0]
 while True:
     clock.tick(15)
     screen.fill((0,0,0))
@@ -46,35 +46,43 @@ while True:
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_SPACE:
                 v=-15
-    toppipe.move_ip(-5,0)
-    bottompipe.move_ip(-5,0)
+    #toppipe.move_ip(-5,0)
+    #bottompipe.move_ip(-5,0)
     for toppipe in tlist:
         if toppipe.x<-50:
             tlist.remove(toppipe)
             o=random.randint(1,10)
             if o<=5:            
-                for i in range(3):
-                    pipe_x=random.randint(288,400)
-                    s[i]=pipe_x
-                    toppipe_y=random.randint(0,300)
-                    t[i]=toppipe_y
-                    toppipe=pygame.Rect((pipe_x,0),(40,toppipe_y))
-                    tlist.append(toppipe)
                 
-                #topimage=pygame.Surface((40,toppipe_y))
+                pipe_x=random.randint(288,400)
+                s[i]=pipe_x
+                toppipe_y=random.randint(0,300)
+                t[i]=toppipe_y
+                toppipe=pygame.Rect((pipe_x,0),(40,toppipe_y))
+                tlist.append(toppipe)
+                topimage=pygame.Surface((40,toppipe_y))
+                topimage.fill((255,255,255))
+        toppipe.move_ip(-5,0)
+    for toppipe in tlist:
+        screen.blit(topimage,toppipe)
+                
+                
                 #bottomimage=pygame.Surface((40,bottompipe_y))
                 #bottomimage.fill((255,255,255))
-                #topimage.fill((255,255,255))
+                
     for bottompipe in blist:
         if bottompipe.x<-50:
             blist.remove(bottompipe)
             z=random.randint(1,10)
             if z<=5:
-                for i in range (3):
-                    print (s[i])
-                    bottompipe_y=362-toppipe_y
-                    bottompipe=pygame.Rect((s[i],512-t[i]),(40,t[i]))
-                    blist.append(bottompipe)
+                
+                print (s[i])
+                bottompipe_y=362-toppipe_y
+                bottompipe=pygame.Rect((s[i],512-t[i]),(40,t[i]))
+                blist.append(bottompipe)
+                bottomimage=pygame.Surface((40,bottompipe_y))
+                bottomimage.fill((255,255,255))
+        bottompipe.move_ip(-5,0)
     if player.colliderect(toppipe) or player.colliderect(bottompipe):
         print("GAME OVER!")
         quit()
@@ -82,8 +90,6 @@ while True:
     player.move_ip(0,v)
     #print(v)
 
-    for toppipe in tlist:
-        screen.blit(topimage,toppipe)
     for bottompipe in blist:
         screen.blit(bottomimage,bottompipe)
     
